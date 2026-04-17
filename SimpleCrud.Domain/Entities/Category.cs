3f702d7e7a2067889a2d8aa174b59202e6128981
Niyaa1786 <j4code1786@gmail.com>
2026-04-17 13:36:00 +0700

    feat(domain): add Category and Product entities

---
+++ SimpleCrud.Domain/Entities/Category.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCrud.Domain.Entities
+{
+    public class Category
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }= string.Empty;
+        private List<Product> _products = new();
+        public IReadOnlyCollection<Product> Products => _products.AsReadOnly();
+
+        public Category(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }
+
+        private Category() { }
+
+        public void Update(string name)
+        {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException("name");
+            
+            Name = name;
+        }
+
+
+
+
+    }
+}
---
+++ SimpleCrud.Domain/Entities/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCrud.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string? Name { get; private set; }
+        public decimal Price { get; private set; }
+        public Guid CategoryId { get; private set; }
+        public Category Category { get; private set; }
+
+        public Product(string name, decimal price, Guid categoryId)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Price = price;
+            CategoryId = categoryId;
+        }
+
+        public Product() { }
+
+        public void Update(string name, decimal price, Guid categoryId)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name");
+            if (price < 0)
+                throw new ArgumentOutOfRangeException("Price cannot be negative.");
+            
+            Name = name;
+            Price = price;
+            CategoryId = categoryId;
+        }
+    }
+}
