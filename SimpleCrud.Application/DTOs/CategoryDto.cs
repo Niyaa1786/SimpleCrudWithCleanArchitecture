9ffba43aad03d2072040a1299fb3f2767000df52
Niyaa1786 <j4code1786@gmail.com>
2026-04-18 16:12:12 +0700

    feat(application): add CategoryDto and ProductDto

---
+++ SimpleCrud.Application/DTOs/CategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCrud.Application.DTOs
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
---
+++ SimpleCrud.Application/DTOs/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using SimpleCrud.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCrud.Application.DTOs
+{
+    public class ProductDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
