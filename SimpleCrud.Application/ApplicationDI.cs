3cf91713869063fdb54c5539700105877276f1e0
Niyaa1786 <j4code1786@gmail.com>
2026-04-22 01:33:45 +0700

    feat(application): update ApplicationDI.cs

--- SimpleCrud.Application/ApplicationDI.cs
+++ SimpleCrud.Application/ApplicationDI.cs
@@ -1,6 +1,8 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using SimpleCrud.Application.UseCases.Categories;
+using SimpleCrud.Application.UseCases.Products;
 using SimpleCrud.Application.Validators;
 using System;
 using System.Collections.Generic;
@@ -12,6 +14,18 @@ namespace SimpleCrud.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services.AddScoped<CreateCategory>();
+            services.AddScoped<GetAllCategories>();
+            services.AddScoped<GetCategoryById>();
+            services.AddScoped<UpdateCategory>();
+            services.AddScoped<DeleteCategory>();
+
+            services.AddScoped<CreateProduct>();
+            services.AddScoped<GetAllProducts>();
+            services.AddScoped<GetProductById>();
+            services.AddScoped<GetAllProductsByCategory>();
+            services.AddScoped<UpdateProduct>();
+            services.AddScoped<DeleteProduct>();
             // Register all validators in the assembly
             services.AddValidatorsFromAssemblyContaining<IValidatorMarker>();
             return services;
